d5772c778419b8629197d7e897706acdbf01c0c3
Aaron Bowers <bowersaaronjames@gmail.com>
2022-04-18 20:19:50 -0700

    Answer Grub subclass prompts.

--- src/RetiredForagerBee.js
+++ src/RetiredForagerBee.js
@@ -1,3 +1,14 @@
-class RetiredForagerBee {
-  // TODO..
-};
+class RetiredForagerBee extends ForagerBee {
+  constructor() {
+    super();
+    this.age = 40;
+    this.color = 'grey';
+    this.job = 'gamble';
+    this.canFly = false;
+    this.treasureChest = [];
+    this.forage('I am getting to old, let me play cards instead');
+  }
+  gamble(treasure) {
+    return super.forage(treasure);
+  }
+}
